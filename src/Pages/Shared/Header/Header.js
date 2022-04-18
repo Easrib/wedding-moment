1379824e64bd8a9c682f3fd722cf60b5c2600dea
Easrib <easrib.sharik01@gmail.com>
2022-04-18 11:26:13 +0600

    sign out added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignout = () => {
+        signOut(auth);
+    }
     return (
         <Navbar sticky='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -18,9 +25,12 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
-                        <Nav.Link as={Link} to="/login">
-                            Login
-                        </Nav.Link>
+                        {user ?
+                            <button onClick={handleSignout}>Sign Out</button>
+                            :
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
